d55780e8962758b1e79430dfc745825481a341ae
sakshamsharma25113 <25113ew018@glbitm.ac.in>
2026-06-05 18:35:05 +0530

    Day-3 uploaded

---
+++ Day-5/factors.c
@@ -0,0 +1,19 @@
+//Write a program to Print factors of a number.
+#include<stdio.h>
+int main()
+
+{
+int n,i;
+
+printf("Enter the number:");
+scanf("%d",&n);
+
+printf("The factors are:");
+for(i=1;i<=n;i++)
+{
+    if(n%i==0)
+    printf("\t%d",i);
+}
+
+    return 0;
+}
