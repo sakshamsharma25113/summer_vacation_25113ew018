e239ab1948f23f683403fd5216298b045ea19dd3
sakshamsharma25113 <25113ew018@glbitm.ac.in>
2026-06-11 19:13:38 +0530

    Day-9 uploaded

---
+++ Day-9/reverseNumTriangle.c
@@ -0,0 +1,15 @@
+//Write a program to Print reverse number triangle
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 5; i >= 1; i--) {
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
