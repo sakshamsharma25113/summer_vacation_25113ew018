2f9d57892f80238f7eefb1d1b04ecbb930214291
sakshamsharma25113 <25113ew018@glbitm.ac.in>
2026-06-30 20:48:35 +0530

    Day-24 uploaded

---
+++ Day-24/longestWord.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200], word[100], longest[100];
+    int i = 0, j = 0, max = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(1) {
+        if(str[i] == ' ' || str[i] == '\0' || str[i] == '\n') {
+            word[j] = '\0';
+
+            if(j > max) {
+                max = j;
+                strcpy(longest, word);
+            }
+
+            j = 0;
+
+            if(str[i] == '\0' || str[i] == '\n')
+                break;
+        } else {
+            word[j++] = str[i];
+        }
+
+        i++;
+    }
+
+    printf("Longest word = %s\n", longest);
+
+    return 0;
+}
