fcb47d047515c17b0ed45fd5424393b9ef8c41e4
sakshamsharma25113 <25113ew018@glbitm.ac.in>
2026-06-30 20:17:57 +0530

    Day-16 uploaded

---
+++ Day-16/missingNo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    int sum = 0, total;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n - 1);
+    for(i = 0; i < n - 1; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    total = n * (n + 1) / 2;
+
+    printf("Missing number = %d\n", total - sum);
+
+    return 0;
+}
